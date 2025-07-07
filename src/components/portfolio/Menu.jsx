ed95ea7f2f34e18fd1615658ec3b114d7a81c87c
Vageshwari Chaudhary <104719093+Vage10@users.noreply.github.com>
2025-07-07 08:05:41 +0530

    Update Menu.jsx

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -1,19 +1,45 @@
-import Work1 from "../../assets/music.jpg";
-import Work2 from "../../assets/alzheimer.jpg";
-import Work3 from "../../assets/chat.jpg";
-import Work4 from "../../assets/stack.jpg";
+import Work4 from "../../assets/music.jpg";
+import Work5 from "../../assets/alzheimer.jpg";
+import Work2 from "../../assets/resume.jpg";
+import Work3 from "../../assets/job.jpg";
+import Work6 from "../../assets/stack.jpg";
+import Work1 from "../../assets/doctor.jpg";
 
 const Menu = [
   {
     id: 1,
     image: Work1,
+    title: "Medimeet - A Doctor's Appointment App",
+    category: ["Frontend", "Backend", "React"],
+    url: "https://doctors-appointment-vage.vercel.app/",
+    repositoryUrl: "https://github.com/Vage10/Doctors-appointment",
+  },
+  {
+    id: 2,
+    image: Work2,
+    title: "Sensai - AI-powered resume builder",
+    category: ["Frontend", "Backend", "React"],
+    url: "https://sensai-vage.vercel.app/",
+    repositoryUrl: "https://github.com/Vage10/sensai",
+  },
+  {
+    id: 3,
+    image: Work3,
+    title: "A Job Portal App",
+    category: ["Frontend", "React"],
+    url: "https://job-portal-vage.vercel.app/",
+    repositoryUrl: "https://github.com/Vage10/job-portal",
+  },
+  {
+    id: 4,
+    image: Work4,
     title: "React Music App",
     category: ["Frontend", "React"],
     repositoryUrl: "https://github.com/Vage10/music",
   },
   {
-    id: 2,
-    image: Work2,
+    id: 5,
+    image: Work5,
     title: "Alzheimer's disease detection system",
     category: ["Backend", "Python"],
     //url: "https://cryptoverse.gregsithole.com",
@@ -21,16 +47,8 @@ const Menu = [
       "https://github.com/Vage10/Alzheimer-s-disease-detection-system",
   },
   {
-    id: 3,
-    image: Work3,
-    title: "Chatting App",
-    category: ["Frontend, React"],
-    // url: "https://travel.gregsithole.com",
-    repositoryUrl: "https://github.com/Vage10/BlinkChat",
-  },
-  {
-    id: 4,
-    image: Work4,
+    id: 6,
+    image: Work6,
     title: "StackOverFlow Clone",
     category: ["Frontend"],
     //url: "https://www.npmjs.com/package/wallhaven-cli",
